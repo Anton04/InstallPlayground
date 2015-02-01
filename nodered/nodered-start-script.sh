6b079150852ac7380d6776a202303acb29d88402
Anton04 <anton.gustafsson@tii.se>
2015-02-01 15:30:02 +0100

    Update nodered-start-script.sh

--- nodered/nodered-start-script.sh
+++ nodered/nodered-start-script.sh
@@ -20,7 +20,7 @@
  USERNAME='iot'
  APP_PATH='/home/iot/node-red'
  HISTORY=1024
- INVOCATION="source ~/.nvm/nvm.sh && nvm run 0.8.22 $SERVICE" 
+ INVOCATION="source /home/iot/.nvm/nvm.sh && nvm run 0.8.22 $SERVICE" 
  ME=`whoami`
 
  as_user() {
