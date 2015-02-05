68341f3272d5fcc132c26ec6b8051246f82cab3c
Anton04 <anton.gustafsson@tii.se>
2015-02-05 17:31:08 +0100

    Create install_node_red.sh

---
+++ nodered/install_node_red.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+
+source source ~/.nvm/nvm.sh
+
+nvm install 0.8.22
+nvm use 0.8.22
+
+wget https://github.com/node-red/node-red/archive/0.9.1.zip
+unzip 0.9.1.zip
+rm 0.9.1.zip
+
+ln -s node-red* node-red
+
+cd node-red 
+
+npm install --production
+
