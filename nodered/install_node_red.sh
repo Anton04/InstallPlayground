0078d5a3f6c6a1c2904cc841d173c375c9b7c2a1
Anton04 <anton.gustafsson@tii.se>
2015-02-05 22:57:40 +0100

    Update install_node_red.sh

--- nodered/install_node_red.sh
+++ nodered/install_node_red.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+node_version='0.8.22'
+red_version='0.9.1'
+
+
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
-nvm install 0.8.22
-nvm use 0.8.22
+nvm install $node_version
+nvm use $node_version
+
+
 
 wget https://github.com/node-red/node-red/archive/0.9.1.zip
-unzip 0.9.1.zip
-rm 0.9.1.zip
+unzip $red_version.zip
+rm $red_version.zip
 
-ln -s node-red* node-red
+ln -s node-red-$red_version node-red
 
 cd node-red 
 
