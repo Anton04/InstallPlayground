6c2c3c6a9094926e562db566248e007dd2e565e6
Anton04 <anton.gustafsson@tii.se>
2015-02-19 23:11:29 +0100

    Update node_install.sh

--- nodered/node_install.sh
+++ nodered/node_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+node_version='0.8.22'
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
 
+
 . ~/.nvm/nvm.sh
 
-nvm install 0.8.22
-nvm use 0.8.22
+nvm install $node_version
+nvm use $node_version
