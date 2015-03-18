dea4a7d8ce165e59d9f6e3a480f8283d76959f5a
Anton04 <anton.gustafsson@tii.se>
2015-03-18 23:23:15 +0100

    Update SetupOnUbuntu.sh

--- SetupOnUbuntu.sh
+++ SetupOnUbuntu.sh
@@ -9,6 +9,7 @@ fi
 apt-get update
 apt-get install git
 
+echo "Creating IoT user"
 
 adduser iot --disabled-password --gecos "Internet of things services and applications" 
 
@@ -20,19 +21,28 @@ sudo -H -u iot bash -c 'mkdir repos'
 #sudo -H -u iot bash -c 'cd repos && git clone https://github.com/Anton04/InstallPlayground.git'
 
 #Install mqtt
+echo "Installing mosquitto"
 apt-get install mosquitto
 
 #Install ipython
+echo "Installing iPython"
 apt-get install ipython-notebook
 
+echo "Installing node.js"
 sudo -H -u iot bash -c 'https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/node_install.sh'
 
 #Install nodered
+echo "Installing NodeRED"
 sudo -H -u iot bash -c 'wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install_node_red.sh | bash'
 
 #Install startup script for nodered
 wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install-node-red-start-script | bash
 
 # Install influxdb 
+echo "Installing influxDB"
 wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 dpkg -i influxdb_latest_amd64.deb
+
+echo "DONE!"
+
+
