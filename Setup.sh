103ae5b4a9a63816f614c6efdb8d2d1568a5af6a
Anton04 <anton.gustafsson@tii.se>
2015-03-18 19:40:00 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,25 @@
 
 #!/bin/bash
 
+apt-get update
+apt-get install git
+
+
 sudo adduser iot 
 
-sudo su iot
+#sudo su iot
 
+#cd ~
+
+sudo -H -u otheruser bash -c 'mkdir repos'
+#sudo -H -u iot bash -c 'cd repos && git clone https://github.com/Anton04/InstallPlayground.git'
+
+#Install mqtt
+apt-get install mosquitto
+
+#Install ipython
+apt-get install ipython-notebook
+
+
+#Install nodered
+sudo -H -u iot bash -c '
