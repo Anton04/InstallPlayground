7003257a879e63d214ae65ea30d34fd892d72986
Anton04 <anton.gustafsson@tii.se>
2015-03-18 23:40:57 +0100

    Update SetupOnUbuntu.sh

--- SetupOnUbuntu.sh
+++ SetupOnUbuntu.sh
@@ -22,17 +22,29 @@ sudo -H -u iot bash -c 'mkdir repos'
 #sudo -H -u iot bash -c 'cd repos && git clone https://github.com/Anton04/InstallPlayground.git'
 
 #Install mqtt
+echo " "
+echo " "
+echo " "
 echo "Installing mosquitto"
 apt-get install mosquitto
 
 #Install ipython
+echo " "
+echo " "
+echo " "
 echo "Installing iPython"
 apt-get install ipython-notebook
 
+echo " "
+echo " "
+echo " "
 echo "Installing node.js"
 sudo -H -u iot bash -c 'https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/node_install.sh'
 
 #Install nodered
+echo " "
+echo " "
+echo " "
 echo "Installing NodeRED"
 sudo -H -u iot bash -c 'wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install_node_red.sh | bash'
 
@@ -40,10 +52,16 @@ sudo -H -u iot bash -c 'wget -qO- https://raw.githubusercontent.com/Anton04/Inst
 wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install-node-red-start-script | bash
 
 # Install influxdb 
+echo " "
+echo " "
+echo " "
 echo "Installing influxDB"
 wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 dpkg -i influxdb_latest_amd64.deb
 
+echo " "
+echo " "
+echo " "
 echo "DONE!"
 
 
