bbebe99543177fdae0732dee1d4e89c541906290
Anton04 <anton.gustafsson@tii.se>
2015-09-12 16:39:24 +0200

    Remove blanks

--- SetupOnUbuntu.sh
+++ SetupOnUbuntu.sh
@@ -45,16 +45,10 @@ adduser iot --disabled-password --gecos "Internet of things services and applica
 
 
 #Install screen
-echo " "
-echo " "
-echo " "
 #echo "Installing screen"
 apt-get -y install screen > /dev/null < /dev/null
 
 #Install mqtt
-echo " "
-echo " "
-echo " "
 echo "Installing mosquitto"
 apt-get -y install mosquitto > /dev/null < /dev/null
 apt-get -y install mosquitto-clients > /dev/null < /dev/null
@@ -66,9 +60,7 @@ pip install mosquitto > /dev/null < /dev/null
 #sudo -H -u iot bash -c 'cd /home/iot && wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/node_install.sh | bash'
 
 #Install nodered
-echo " "
-echo " "
-echo " "
+
 echo "Installing NodeRED"
 sudo -H -u iot bash -c 'cd /home/iot && wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install_node_red.sh | bash'
 
@@ -76,24 +68,18 @@ sudo -H -u iot bash -c 'cd /home/iot && wget -qO- https://raw.githubusercontent.
 wget -qO- https://raw.githubusercontent.com/Anton04/InstallPlayground/master/nodered/install-node-red-start-script | bash
 
 # Install influxdb 
-echo " "
-echo " "
-echo " "
 echo "Installing influxDB"
 wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb > /dev/null
 dpkg -i influxdb_latest_amd64.deb > /dev/null
 
-echo " "
-echo " "
+
 echo "Installing grafana"
 wget https://grafanarel.s3.amazonaws.com/builds/grafana_2.1.3_amd64.deb
 apt-get install -y adduser libfontconfig > /dev/null < /dev/null
 dpkg -i grafana_2.1.3_amd64.deb > /dev/null < /dev/null
-echo "  "
+
 #Install ipython
-echo " "
-echo " "
-echo " "
+
 echo "Installing iPython"
 apt-get -y install ipython-notebook > /dev/null < /dev/null
 
