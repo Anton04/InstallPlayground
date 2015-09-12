b0f0d51889fde399ce3c623f40efa56befa40c75
Anton04 <anton.gustafsson@tii.se>
2015-09-12 16:50:47 +0200

    Update node_install.sh

--- nodered/node_install.sh
+++ nodered/node_install.sh
@@ -2,7 +2,7 @@
 node_version='0.12.27'
 
 #Check if node version manager is installed.
-if  [! -f ~/.nvm/nvm.sh ] ; then
+if  [ ! -f ~/.nvm/nvm.sh ] ; then
   echo "Installing node version manager - to let you easilly switch between different versions of node"
   wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
   . ~/.nvm/nvm.sh
